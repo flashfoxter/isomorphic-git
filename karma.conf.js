2aea9ab13003eebfdcf2f4c5d8011a144ee78185
William Hilton <wmhilton@gmail.com>
2019-11-16 18:58:13 -0500

    bump idleTimeout

--- karma.conf.js
+++ karma.conf.js
@@ -119,7 +119,8 @@ module.exports = function (config) {
       tags: [ISSUE],
       recordScreenshots: false,
       recordVideo: false,
-      public: 'public restricted'
+      public: 'public restricted',
+      idleTimeout: 4 * 60, // default 90
     },
     concurrency: 5,
     // Continuous Integration mode
@@ -127,11 +128,10 @@ module.exports = function (config) {
     singleRun: true,
     // test results reporter to use
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['browsers', 'verbose', 'longest', 'pr-comment', 'junit'],
+    reporters: ['browsers', 'verbose', 'pr-comment', 'junit'],
     junitReporter: {
       outputDir: './junit'
     },
-    longestSpecsToReport: 50,
     browserify: {
       transform: [
         // Replace process.env.CI
